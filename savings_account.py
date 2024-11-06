c608d6da07be72349db5a559767c34423778dd73
tonyd1002 <tonyd1002@gmail.com>
2024-11-05 18:19:57 -0600

    Update savings_account.py

--- savings_account.py
+++ savings_account.py
@@ -1,5 +1,5 @@
-# Import the Account class from the Account.py file
-from Account import Account
+# Import the Account class from the Accounts.py file
+from Accounts import Accounts
 
 # Define a function for the Savings Account
 def create_savings_account(balance, interest_rate, months):
